9adbb41d7b4d801ad07c8fd1db9fc393638f0bf1
HosamHegly <hossam_one@hotmail.com>
2024-03-11 14:15:35 +0200

    "changed testrunner name"

--- tests/ui_tests/add_food_to_meal_test_runner.py
+++ tests/ui_tests/add_food_to_meal_test_runner.py
@@ -8,22 +8,20 @@ from tests.ui_tests.test_runner import get_filename
 from weight_goal_test import WeightGoalTest
 from nutritional_target_input_values_test import *  # Import the tests case
 from target_creation_test import  CreateNutritionalTargetsTest
-
+from Utils.json_reader import get_config_data
 test_cases = [CreateNutritionalTargetsTest, LoginPageTest, MealEditTest, FoodSearchPopupTest, WeightGoalTest,
               NutritionalTargetsValuesTest]
 serial_cases = []
 
 
 def run_tests_for_browser(browser):
-    MealEditTest.browser = browser
-    test_suite = unittest.TestLoader().loadTestsFromTestCase(MealEditTest)
+    LoginPageTest.browser = browser
+    test_suite = unittest.TestLoader().loadTestsFromTestCase(LoginPageTest)
     unittest.TextTestRunner().run(test_suite)
 
 
 if __name__ == "__main__":
-    filename = get_filename("../config.json")
-    with open(filename, 'r') as file:
-        config = json.load(file)
+    config = get_config_data()
     is_parallel = config["parallel"]
     is_serial = config["serial"]
     browsers = config["browser_types"]
